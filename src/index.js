b5fc572f8d2ff7540b088fc435ae9b22b77fb6b5
Hunter Guimont <hunter_guimont@outlook.com>
2020-12-15 12:54:26 -0700

    Basic command handler added

---
+++ src/index.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const fs = require('fs');
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+const prefix = 'h.'
+
+const commandFiles = fs.readdirSync('./src/commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
+
+
+client.on('message', message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (!client.commands.has(command)) return;
+
+	try {
+		client.commands.get(command).execute(message, args);
+	} catch (error) {
+		console.error(error);9
+		message.reply('there was an error trying to execute that command!');
+	}
+});
+
+client.on('ready', () => {
+	console.log(`Logged in as: ${client.user.tag}.`);
+});
+
+client.login(process.env.TOKEN);
