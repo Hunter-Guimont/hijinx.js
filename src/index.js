56199db0946c0916d393a5d5b9c6526ec553087e
Hunter Guimont <hunter_guimont@outlook.com>
2020-12-16 18:18:16 -0700

    Removed event handling and refactored code

--- src/index.js
+++ src/index.js
@@ -1,38 +1,17 @@
-require('dotenv').config();
-const fs = require('fs');
+const config = require('../config.json');
+const Client = require('./classes/client.js');
 
-const Discord = require('discord.js');
-const client = new Discord.Client();
-client.commands = new Discord.Collection();
 
-const prefix = 'h.'
+const client = new Client(config);
 
-const commandFiles = fs.readdirSync('./src/commands').filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	client.commands.set(command.name, command);
+function init() {
+	client.loadCommands();
+	client.loadEvents();
+	client.login(client.token);
 }
 
-
-client.on('message', message => {
-	if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-	const args = message.content.slice(prefix.length).trim().split(/ +/);
-	const command = args.shift().toLowerCase();
-
-	if (!client.commands.has(command)) return;
-
-	try {
-		client.commands.get(command).execute(message, args);
-	} catch (error) {
-		console.error(error);9
-		message.reply('there was an error trying to execute that command!');
-	}
-});
+init();
 
 client.on('ready', () => {
-	console.log(`Logged in as: ${client.user.tag}.`);
+	console.log(`Successfully logged in as ${client.user.tag}.`);
 });
-
-client.login(process.env.TOKEN);
